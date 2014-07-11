275e73e6d17b064b91de294fe094dab2e384c6cb
cwjpalmer <cwjpalmer@gmail.com>
2014-07-11 10:36:22 -0300

    creates a nested object in the library

---
+++ objective-lib-expt/lib/Awesome/Awesome.cpp
@@ -0,0 +1,19 @@
+/*
+  Awesome.cpp library - for reading Awesomes
+  Created by Christopher W. J. Palmer, 26 May 2014
+  Part of the BrewBox Project
+  Private library, not for public use.
+*/
+
+#include "Arduino.h"
+#include "Awesome.h"
+
+Awesome::Awesome()
+{
+
+}
+
+class box {
+  public:
+    int length;
+};
