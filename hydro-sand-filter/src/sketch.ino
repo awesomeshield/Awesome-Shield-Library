54a4dea5e5656d356635afbf4f8cf3ecf391fff0
cwjpalmer <cwjpalmer@gmail.com>
2014-07-12 14:23:29 -0300

    sand filter control takes two float inputs and controls downstream solenoid

--- hydro-sand-filter/src/sketch.ino
+++ hydro-sand-filter/src/sketch.ino
@@ -1,8 +1,41 @@
+#include <Awesome.h>
 
-void setup()
-{
+Awesome easy;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting...");
 }
 
-void loop()
-{
+void openDrain() {
+  easy.rgbLED.turnOn(WHITE);    // make it control relay for drain valve
+}
+
+void closeDrain() {
+  easy.rgbLED.turnOn(RED);    // make it control relay for drain valve
+}
+
+void loop() {
+  Serial.print("Looping.");
+
+  bool upperFloatSubmerged = easy.button.read();  // make it read float switch
+  bool lowerFloatSubmerged = easy.toggleSwitch.read();
+
+  Serial.print("Upper Float: ");
+  Serial.println(upperFloatSubmerged);
+  Serial.print("Low Float: ");
+  Serial.println(lowerFloatSubmerged);
+
+  if ( lowerFloatSubmerged ) {
+    openDrain();
+  } else if ( upperFloatSubmerged ) {
+    Serial.print("Error: float malfunction. Upper float reads submerged, lower float reads not submerged. Check float switches. One may be stuck.");
+    closeDrain();
+  } else {
+    Serial.print("Both float switches read not submerged. Check filter to ensure sand bed is not dry.");
+    closeDrain();
+  }
+
+  // just to keep things from running to fast
+  delay(50);
 }
