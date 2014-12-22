f771adcf2a2b30ab5698b4aadbc5a3a7f16af6f3
cwjpalmer <cwjpalmer@gmail.com>
2014-12-22 07:39:18 +0100

    reworked variable names

--- lib/Awesome/examples/lovemeter/lovemeter.ino
+++ lib/Awesome/examples/lovemeter/lovemeter.ino
@@ -2,9 +2,9 @@
 Awesome awesome;
 
 float temp;
-float lowestTemp;     // should be air temp
+float airTemp;     // should be air temp
 float highestTemp;
-int tempInterval = 3;
+int tempInterval = 5.5;
 int red;      // change to hotness
 int blue;     // change to coldness
 float howHotIsIt; // change to hotter than air
@@ -13,8 +13,8 @@ float howHotIsIt; // change to hotter than air
 void setup() {
   Serial.begin(9600);
   //awesome.buzzer.setSilent(true);
-  lowestTemp = awesome.temperatureSensor.read() - 0.25;
-  highestTemp = lowestTemp + tempInterval;
+  airTemp = awesome.temperatureSensor.read() - 0.25;
+  highestTemp = airTemp + tempInterval;
 }
 
 void loop() {
@@ -30,12 +30,12 @@ void loop() {
   // we particularly need to know how much hotter than room temperature it is
   // [ ] break this into two variables: 'hotter than air temp' and 'maximum hotter than air temp'
   // explain each value, and the use of their ratio
-  howHotIsIt = (temp - lowestTemp ) / ( highestTemp - lowestTemp );
+  howHotIsIt = (temp - airTemp ) / ( highestTemp - airTemp );
 
   // if temp < lowest temp [ ] change
-  if ( howHotIsIt < 0 ) {
+  if ( temp < airTemp ) {
     // adjust air temp ... the air got colder
-    lowestTemp = temp - 0.25;
+    airTemp = temp - 0.25;
     // since the temperature is colder than we expected, me
     red = 0;
     } else if ( howHotIsIt < 1 ) {
@@ -59,5 +59,5 @@ void loop() {
     // we want the ticks to be farther apart when the temperature is lower (colder)
     // the number we found for how much blue light we want does the same thing
     // the amount of blue is bigger when the sensor is colder
-    delay(blue);
+    delay(blue*4);
   }
