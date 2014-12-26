1dcdfb1ffc0b7671db76cae6fa40435f8186b2bd
cwjpalmer <cwjpalmer@gmail.com>
2014-12-26 16:18:38 -0500

    simplified, building from scratch for tutorial

--- lib/Awesome/examples/lovemeter/lovemeter.ino
+++ lib/Awesome/examples/lovemeter/lovemeter.ino
@@ -1,63 +1,19 @@
 #include <Awesome.h>
 Awesome awesome;
 
-float temp;
-float airTemp;     // should be air temp
-float highestTemp;
-int tempInterval = 5.5;
-int red;      // change to hotness
-int blue;     // change to coldness
-float howHotIsIt; // change to hotter than air
+float sensorTemp;
+float airTemp;
+float howMuchHotterThanAir;
+float red;
 
 
 void setup() {
-  Serial.begin(9600);
-  //awesome.buzzer.setSilent(true);
-  airTemp = awesome.temperatureSensor.read() - 0.25;
-  highestTemp = airTemp + tempInterval;
+  airTemp = awesome.temperatureSensor.check();
 }
 
 void loop() {
-  // we're going to need to use the current temperature in our calculations
-  // so let's store it in a variable called 'temp'
-  // I picked this name because it's short, and reminds me what the variable means
-  temp = awesome.temperatureSensor.read();
-  /*Serial.print("temp = ");
-  Serial.println(temp);*/
-
-  // mathematically, this interpolation.. that's gonna be a DOOZEY to explain
-  // we need to measure how hot it is
-  // we particularly need to know how much hotter than room temperature it is
-  // [ ] break this into two variables: 'hotter than air temp' and 'maximum hotter than air temp'
-  // explain each value, and the use of their ratio
-  howHotIsIt = (temp - airTemp ) / ( highestTemp - airTemp );
-
-  // if temp < lowest temp [ ] change
-  if ( temp < airTemp ) {
-    // adjust air temp ... the air got colder
-    airTemp = temp - 0.25;
-    // since the temperature is colder than we expected, me
-    red = 0;
-    } else if ( howHotIsIt < 1 ) {
-      red = howHotIsIt * 100;
-      } else {
-        red = 100;
-      }
-
-    blue = 100 - red;
-
-    red = red * 2.5;
-    blue = blue * 2.5;
-
-    // make a tick noise, adjust the length to what works for you
-    awesome.buzzer.beep(5);
-
-    // turn on the RGB with the right amount of red and blue
-    awesome.rgbLED.turnOn(red,0,blue);
-
-    // the longer this delay is, the farther apart the tick noises will be
-    // we want the ticks to be farther apart when the temperature is lower (colder)
-    // the number we found for how much blue light we want does the same thing
-    // the amount of blue is bigger when the sensor is colder
-    delay(blue*4);
-  }
+  sensorTemp = awesome.temperatureSensor.check();
+  howMuchHotterThanAir = sensorTemp - airTemp;
+  red = howMuchHotterThanAir * 100;
+  awesome.colorLED.turnOn(red, 0, 0);
+}
