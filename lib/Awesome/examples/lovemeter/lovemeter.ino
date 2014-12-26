925b90d48e5b742049bd359356f00a6d22c81cf6
cwjpalmer <cwjpalmer@gmail.com>
2014-12-26 17:09:54 -0500

    maxed out LED inputs

--- lib/Awesome/examples/lovemeter/lovemeter.ino
+++ lib/Awesome/examples/lovemeter/lovemeter.ino
@@ -15,12 +15,12 @@ void setup() {
 void loop() {
   sensorTemp = awesome.temperatureSensor.check();
   howMuchHotterThanAir = sensorTemp - airTemp;
-  red = howMuchHotterThanAir * 10;
-  blue = 100 - red;
+  red = howMuchHotterThanAir * 100;
+  blue = 1000 - red;
 
   awesome.buzzer.beep(10);
 
   awesome.colorLED.turnOn(red, 0, blue);
 
-  delay(blue*10);
+  delay(blue);
 }
