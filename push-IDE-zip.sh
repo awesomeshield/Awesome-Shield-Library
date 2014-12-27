aa0fac52cc001eab4ed6a102ca5ba9cfb6317376
cwjpalmer <cwjpalmer@gmail.com>
2014-12-27 14:01:41 -0500

    shell script now creates zip; zip not commited to git

--- push-IDE-zip.sh
+++ push-IDE-zip.sh
@@ -2,4 +2,6 @@
 
 cp -R -p /Volumes/BOOSTER/Dropbox/WC/awesome-board/lib/Awesome /Users/Chris/Documents/Arduino/libraries/
 
+tar -zcvf Awesome.zip /Volumes/BOOSTER/Dropbox/WC/awesome-board/lib/Awesome/
+
 echo "Finished"
