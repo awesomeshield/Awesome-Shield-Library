89c8bfb46e3af033d7a8386cf52451f86e9fa68a
cwjpalmer <cwjpalmer@gmail.com>
2015-06-03 11:44:00 +0200

    new empty sketch

---
+++ Awesome/examples/battle-dinghy/battle-dinghy.ino
@@ -0,0 +1,18 @@
+// the next line tells the Arduino you're using the Awesome Shield library
+#include <Awesome.h>
+// the next line tells the Arduino you're using the Awesome Shield itself
+Awesome awesome;
+
+
+// declare variables here
+
+
+void setup()
+{
+  // put code here to have it run right after the Arduino turns on or resets
+}
+
+void loop()
+{
+  // put code here to have it run over and over again
+}
