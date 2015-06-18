674ae0f8550a17a2f4eb517418922f6b29930d6f
cwjpalmer <cwjpalmer@gmail.com>
2015-06-19 00:39:18 +0200

    checks how many times Arduino loops per second using a simple sketch

---
+++ Awesome/examples/how-fast/how-fast.ino
@@ -0,0 +1,21 @@
+/* print the number of times this sketch loops per second
+   using a 10 second sample
+*/
+
+int i;
+
+void setup()
+{
+  // put code here to have it run right after the Arduino turns on or resets
+}
+
+void loop()
+{
+  // put code here to have it run over and over again
+  while ( millis < 10000) {
+    i+=1;
+  }
+  Serial.begin(9600);
+  Serial.println(millis/10);
+  delay(10000);
+}
