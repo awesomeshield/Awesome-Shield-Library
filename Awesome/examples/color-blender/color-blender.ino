a2964a57e1fbc984e33b9f6871768184056d60b3
cwjpalmer <cwjpalmer@gmail.com>
2015-06-23 22:36:29 +0200

    removed interrupt WIP

--- Awesome/examples/color-blender/color-blender.ino
+++ Awesome/examples/color-blender/color-blender.ino
@@ -3,60 +3,46 @@
 // the next line tells the Arduino you're using the Awesome Shield itself
 Awesome awesome;
 
-// color variables
-int red   = 128;
-int green = 128;
-int blue  = 128;
 
-// color select variable
-int colorSelect = 3;
-int state = 0;
-int knobValue;
+// declare variables here
+int red, green, blue;
+bool wasLastPressed = false;
+int counter;
+int currentColor = 3;
 
-void onPress() {
-  colorSelect += 1;
-}
 
-void setup() {
+void setup()
+{
   // put code here to have it run right after the Arduino turns on or resets
   Serial.begin(9600);
-  attachInterrupt(0, onPress, RISING);
 }
 
-void loop() {
-  state = colorSelect % 3;
-  knobValue = awesome.knob.reading()/4;
-
-  Serial.println(state);
-
-  if ( awesome.button.isOn() ) {
-    switch ( state ) {
-      case 0:  // red
-        awesome.LED.turnOn(RED);
-        break;
-      case 1:  // green
-        awesome.LED.turnOn(GREEN);
-        break;
-      case 2:  // blue
-        awesome.LED.turnOn(BLUE);
-        break;
-      default:
-        break;
-    }
-  } else {
-    switch ( state ) {
-      case 0:  // red
-        red = knobValue;
-        break;
-      case 1:  // green
-        green = knobValue;
-        break;
-      case 2:  // blue
-        blue = knobValue;
-        break;
-      default:
-        break;
-    }
-    awesome.LED.turnOn(red,green,blue);
+void loop()
+{
+  // loop counter
+  if ( awesome.button.isOn() && ! wasLastPressed ) {
+    counter += 1;
+    wasLastPressed = true;
   }
+  if ( awesome.button.isOff() ) {
+    wasLastPressed = false;
+  }
+
+  // convert loop counter to ternary value
+  currentColor = counter % 3 + 1;
+
+  switch(currentColor) {
+    case 1:
+      awesome.LED.turnOn(RED);
+      break;
+    case 2:
+      awesome.LED.turnOn(GREEN);
+      break;
+    case 3:
+      awesome.LED.turnOn(BLUE);
+      break;
+  }
+
+  Serial.println(counter);
+  awesome.LED.turnOn(red,green,blue);
 }
