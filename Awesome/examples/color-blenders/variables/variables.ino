712ff7e15f9fec23e428048da864ac77bf96daa0
cwjpalmer <cwjpalmer@gmail.com>
2015-06-25 20:35:25 +0200

    new example, set variables and flash sketch to change colors

---
+++ Awesome/examples/color-blenders/variables/variables.ino
@@ -0,0 +1,22 @@
+// tell the Arduino you're using the Awesome Shield library
+#include <Awesome.h>
+// tell the Arduino you're using the Awesome Shield hardware
+Awesome awesome;
+
+// declare variables here
+
+// change these values to numbers between 0-255
+// and see what colors you can produce :-)
+int red   = 100;
+int green = 100;
+int blue  = 100;
+
+int colorValue;
+
+void setup() {
+  // turn on the LED, using the color variables as inputs
+  awesome.LED.turnOn(red, green, blue);
+}
+
+void loop() {
+}
