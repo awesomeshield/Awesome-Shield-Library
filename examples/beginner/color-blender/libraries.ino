27851ac5a6c50b200ef4b917c356341ea52affa6
Chris Palmer <cwjpalmer@gmail.com>
2016-11-10 10:58:18 +0100

    fixed example filenames

--- examples/beginner/color-blender/libraries.ino
+++
@@ -1,8 +0,0 @@
-// include other libraries to support add-ons
-#include "Arduino.h"
-#include "Awesome.h"
-#include "Wire.h"
-// #include "rgb_lcd.h"
-#include <avr/pgmspace.h>
-#include <Servo.h>
-#include <Adafruit_NeoPixel.h>
